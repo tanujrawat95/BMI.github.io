210cfbda16ca24a0026aec5be88d04f0f74cc469
tanujrawat95 <66406059+tanujrawat95@users.noreply.github.com>
2020-06-04 12:11:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function Calculate() {
+  var weight = document.getElementById("weight").value;
+  var height =  document.getElementById("height").value;
+  var bmi= (weight/height/height)*10000;
+bmi=bmi.toFixed(2);
+if(bmi<18)
+{
+  document.getElementById("result").innerHTML ="Your bmi is "+ bmi +". You need to eat more you thinass. ";
+}
+if(bmi>=18 && bmi<=22)
+{
+  document.getElementById("result").innerHTML ="Your bmi is "+ bmi +". You are fit. Keep it that way ";
+
+}
+if(bmi>24)
+{
+  document.getElementById("result").innerHTML ="Your bmi is "+ bmi +". You need to eat less or you end uo being a lazy ass. ";
+
+}
+}
